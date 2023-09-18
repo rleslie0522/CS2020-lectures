6b866ec5bc2abc94f6f1677f9ff3d32a85468af5
REBEKAH LESLIE <r.leslie.22@abdn.ac.uk>
2023-09-18 15:49:55 +0000

    print options

--- week-one/HelloWorld.java
+++ week-one/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.printf("%s%n%s%n", "Hello", "World");
-        // System.out.println("World");
+        System.out.print("Hello\nWorld\n");
+        System.out.println("Hello World");
     }
 }
